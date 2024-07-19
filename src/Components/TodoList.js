a283a933a2a45e8b6b5c1c0b1ad2131992601e7d
Mosab_Jaber <mosabjabr33@gmail.com>
2024-07-20 00:41:40 +0300

    third commit

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -30,6 +30,7 @@ export default function TodoList() {
   useEffect(() => {
     const storageTodos = JSON.parse(localStorage.getItem("todos"));
     settodos(storageTodos ?? []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   function handleAddClick() {
